5e23de74cee8d2670a9c5689a4838c2daa6b3053
Eric Bowden <bjamese@gmail.com>
2015-01-05 15:34:30 -0700

    upgrade to socket.io 1.0

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-var express = require('express'),
-    app = express()
-  , http = require('http')
-  , server = http.createServer(app)
-  , io = require('socket.io').listen(server);
-    //,util = require('util');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-io.set('log level', 2); // reduce logging
-
-server.listen(3000);
+http.listen(3000);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 var buffer = [];
@@ -19,7 +15,7 @@ io.sockets.on('connection', function(client){
     var clientId = client.id;
     clientId = 'User'+clientId.substr(clientId.length-3,3);
 
-    client.broadcast.emit('connect',clientId + ' connected');
+    client.broadcast.emit('client_connect',clientId + ' connected');
     //client.send(JSON.stringify({'messages':buffer}));
     client.emit('message',{'messages':buffer});
 
@@ -28,12 +24,12 @@ io.sockets.on('connection', function(client){
 
         var msg = { user: clientId, message: message};
         buffer.push(msg);
-        if (buffer.length > 15)
+        while (buffer.length > 15)
         	buffer.shift(); //removes first item in array
         client.broadcast.emit('message',{'messages':[msg]});
     });
 
     client.on('disconnect', function(){
-        client.broadcast.emit('disconnect',clientId + ' disconnected');
+        client.broadcast.emit('client_disconnect',clientId + ' disconnected');
     });
 });
